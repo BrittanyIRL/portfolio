600ce56db783dd29bf0c9316d1db119dc6269c24
Brittany Brassell <brittany.brassell@gmail.com>
2015-12-06 22:32:36 -0800

    adding responsive intro text

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
 // mobile nav
  $(".button-collapse").sideNav();
 
+//headline resizing dynamically 
+
+$("#intro").fitText(0.9, { minFontSize: '16px', maxFontSize: '70px' });
 });
