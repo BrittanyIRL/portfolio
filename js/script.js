5f160e2248db9c2a5f06023a14dcab936657a71b
Brittany Brassell <brittany.brassell@gmail.com>
2016-10-16 11:38:19 -0700

    adding in some colors for external links

--- js/script.js
+++ js/script.js
@@ -10,4 +10,33 @@ $("#intro").fitText(0.9, { minFontSize: '16px', maxFontSize: '70px' });
 
 $(".statementText").fitText(1.4, { minFontSize: '16px', maxFontSize: '55px'});
 
+// external link colors to match their site brands
+$(".external-links li").hover(function(){
+	if($(this).hasClass("resume")){
+		console.log('resume');
+		$(this).find(".fa").css("color", "#ffd400");
+	}
+	else if($(this).hasClass("github")){
+		$(this).find(".fa").css("color", "#6cc644");
+	}
+	else if($(this).hasClass("linkedin")){
+		$(this).find(".fa").css("color", "#0077b5");
+	}
+	else if($(this).hasClass("twitter")){
+		$(this).find(".fa").css("color", "#1DA1F2");
+	}
+	else if($(this).hasClass("instagram")){
+		$(this).find(".fa").css("color", "#833AB4");
+	}
+	else if($(this).hasClass("flickr")){
+		$(this).find(".fa").css("color", "#ff0084");
+	}
+	else if($(this).hasClass("blog")){
+		$(this).find(".fa").css("color", "#d54e21");
+	}
+}, 
+function(){
+	$(this).find(".fa").css("color", "#16FFBD");
+});
+
 });
